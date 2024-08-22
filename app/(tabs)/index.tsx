1390a096ff88aa7bad18e7813f41141d09ebaa5c
s3xmaxman <badside3919@gmail.com>
2024-08-22 17:13:52 +0900

    delete 2 files and update 1 file

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,19 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Link } from "expo-router";
 
 const index = () => {
   return (
     <View>
-      <Text>page</Text>
+      <Link href={"/(modals)/login"}>
+        <Text>Login</Text>
+      </Link>
+      <Link href={"/(modals)/booking"}>
+        <Text>Booking</Text>
+      </Link>
+      <Link href={"/listing/1"}>
+        <Text>Listing</Text>
+      </Link>
     </View>
   );
 };
