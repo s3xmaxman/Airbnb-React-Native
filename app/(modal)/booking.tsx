64c676885ecf69ba0df86cc39d811dc360fb9398
s3xmaxman <badside3919@gmail.com>
2024-08-22 15:17:26 +0900

    update app/(tabs)/index.tsx

--- app/(modal)/booking.tsx
+++ app/(modal)/booking.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const index = () => {
+const Page = () => {
   return (
     <View>
-      <Text>page</Text>
+      <Text>Booking</Text>
     </View>
   );
 };
 
-export default index;
+export default Page;
