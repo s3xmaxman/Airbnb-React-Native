6cb43c2cfea01ae55b3662b9a3c9007548a5fe5f
s3xmaxman <badside3919@gmail.com>
2024-08-23 20:57:38 +0900

    update app/(tabs)/index.tsx, babel.config.js and Listings.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,26 @@
 import { View, Text } from "react-native";
 import React, { useState } from "react";
 import { Link, Stack } from "expo-router";
+import { useMemo } from "react";
 import ExploreHeader from "@/components/ExploreHeader";
 import Listings from "@/components/Listings";
+import listingsData from "@/assets/data/airbnb-listings.json";
 
 const index = () => {
+  const items = useMemo(() => listingsData as any, []);
   const [category, setCategory] = useState<string>("Tiny homes");
 
   const onDataChange = (category: string) => {
     setCategory(category);
   };
   return (
-    <View style={{ flex: 1, marginTop: 200 }}>
+    <View style={{ flex: 1, marginTop: 150 }}>
       <Stack.Screen
         options={{
           header: () => <ExploreHeader onCategoryChanged={onDataChange} />,
         }}
       />
-      <Listings listings={[]} refresh={0} category={category} />
+      <Listings listings={items} refresh={0} category={category} />
     </View>
   );
 };
--- components/Listings.tsx
+++ components/Listings.tsx
@@ -22,12 +22,83 @@ interface Props {
 }
 
 const Listings = ({ listings, refresh, category }: Props) => {
-  useEffect(() => {});
+  const listRef = useRef<BottomSheetFlatListMethods>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 200);
+  }, [category]);
+
+  const renderRow: ListRenderItem<any> = ({ item }) => {
+    return (
+      <Link href={`/listing/${item.id}`} asChild>
+        <TouchableOpacity>
+          <Animated.View
+            style={styles.listing}
+            entering={FadeInRight}
+            exiting={FadeOutLeft}
+          >
+            <Animated.Image
+              source={{ uri: item.medium_url }}
+              style={styles.image}
+            />
+            <TouchableOpacity
+              style={{ position: "absolute", right: 30, top: 30 }}
+            >
+              <Ionicons name="heart-outline" size={24} color="#000" />
+            </TouchableOpacity>
+            <View
+              style={{ flexDirection: "row", justifyContent: "space-between" }}
+            >
+              <Text style={{ fontSize: 16, fontFamily: "mon-sb" }}>
+                {item.name}
+              </Text>
+              <View style={{ flexDirection: "row", gap: 4 }}>
+                <Ionicons name="star" size={16} />
+                <Text style={{ fontFamily: "mon-sb" }}>
+                  {item.review_scores_rating / 20}
+                </Text>
+              </View>
+            </View>
+            <Text style={{ fontFamily: "mon" }}>{item.room_type}</Text>
+            <View style={{ flexDirection: "row", gap: 4 }}>
+              <Text style={{ fontFamily: "mon-sb" }}>€ {item.price}</Text>
+              <Text style={{ fontFamily: "mon" }}>night</Text>
+            </View>
+          </Animated.View>
+        </TouchableOpacity>
+      </Link>
+    );
+  };
+
   return (
-    <View>
+    <View style={defaultStyles.container}>
       <Text>Listings</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  listing: {
+    padding: 16,
+    gap: 10,
+    marginVertical: 16,
+  },
+  image: {
+    width: "100%",
+    height: 300,
+    borderRadius: 10,
+  },
+  info: {
+    textAlign: "center",
+    fontFamily: "mon-sb",
+    fontSize: 16,
+    marginTop: 4,
+  },
+});
 
 export default Listings;
