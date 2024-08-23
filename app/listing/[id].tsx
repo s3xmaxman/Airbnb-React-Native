0b9a1b6d627f913c1e5fe000485c1da36eddc2ef
s3xmaxman <badside3919@gmail.com>
2024-08-23 22:58:50 +0900

    update [id].tsx

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,9 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { useLocalSearchParams } from "expo-router";
+import listingsData from "@/assets/data/airbnb-listings.json";
 
 const Page = () => {
   const { id } = useLocalSearchParams<{ id: string }>();
+
+  const listing = (listingsData as any[]).find((listing) => listing.id === id);
+
   return (
     <View>
       <Text>[id]</Text>
