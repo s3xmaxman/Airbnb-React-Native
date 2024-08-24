7bbc7b8e41e01f1cd339ce8a0ff2984e55a22ead
s3xmaxman <badside3919@gmail.com>
2024-08-24 18:27:29 +0900

    update [id].tsx

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -44,11 +44,89 @@ const Page = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "white",
   },
   image: {
-    width,
     height: IMG_HEIGHT,
+    width: width,
+  },
+  infoContainer: {
+    padding: 24,
+    backgroundColor: "#fff",
+  },
+  name: {
+    fontSize: 26,
+    fontWeight: "bold",
+    fontFamily: "mon-sb",
+  },
+  location: {
+    fontSize: 18,
+    marginTop: 10,
+    fontFamily: "mon-sb",
+  },
+  rooms: {
+    fontSize: 16,
+    color: Colors.grey,
+    marginVertical: 4,
+    fontFamily: "mon",
+  },
+  ratings: {
+    fontSize: 16,
+    fontFamily: "mon-sb",
+  },
+  divider: {
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: Colors.grey,
+    marginVertical: 16,
+  },
+  host: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    backgroundColor: Colors.grey,
+  },
+  hostView: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+  },
+  footerText: {
+    height: "100%",
+    justifyContent: "center",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 4,
+  },
+  footerPrice: {
+    fontSize: 18,
+    fontFamily: "mon-sb",
+  },
+  roundButton: {
+    width: 40,
+    height: 40,
+    borderRadius: 50,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    color: Colors.primary,
+  },
+  bar: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+  },
+  header: {
+    backgroundColor: "#fff",
+    height: 100,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: Colors.grey,
+  },
+
+  description: {
+    fontSize: 16,
+    marginTop: 10,
+    fontFamily: "mon",
   },
 });
 
