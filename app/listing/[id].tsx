230d4693e41a73f2194eff227aad31d215e3c2a4
s3xmaxman <badside3919@gmail.com>
2024-08-24 18:26:32 +0900

    update _layout.tsx and [id].tsx

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,7 +1,28 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams, useNavigation } from "expo-router";
+import React, { useLayoutEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+  Share,
+} from "react-native";
 import listingsData from "@/assets/data/airbnb-listings.json";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
+import Animated, {
+  SlideInDown,
+  interpolate,
+  useAnimatedRef,
+  useAnimatedStyle,
+  useScrollViewOffset,
+} from "react-native-reanimated";
+import { defaultStyles } from "@/constants/Styles";
+
+const { width } = Dimensions.get("screen");
+const IMG_HEIGHT = 300;
 
 const Page = () => {
   const { id } = useLocalSearchParams<{ id: string }>();
@@ -9,10 +30,26 @@ const Page = () => {
   const listing = (listingsData as any[]).find((listing) => listing.id === id);
 
   return (
-    <View>
-      <Text>[id]</Text>
+    <View style={styles.container}>
+      <Animated.ScrollView>
+        <Animated.Image
+          source={{ uri: listing.xl_picture_url }}
+          style={styles.image}
+        />
+      </Animated.ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  image: {
+    width,
+    height: IMG_HEIGHT,
+  },
+});
 
 export default Page;
